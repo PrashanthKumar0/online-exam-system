441854c58bf0c59efc8e4113e33185be7bd5c1ea
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2024-04-26 01:28:52 +0530

    test fxml loading

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,24 +1,32 @@
 package org.example;
 
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    
+
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/login_scene.fxml"));
+            // Parent content = loader.load();
+            VBox vbox = loader.<VBox>load();
+            Scene scene = new Scene(vbox);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void run(){
+    public void run() {
         launch();
     }
 }
