76d52be999a4df086118f01e925c6e8438e74392
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2024-04-27 12:48:23 +0530

    simplified App.js

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,8 +2,8 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -11,11 +11,8 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/login_scene.fxml"));
-            // Parent content = loader.load();
-            VBox vbox = loader.<VBox>load();
-            Scene scene = new Scene(vbox);
+            Parent root = FXMLLoader.load(getClass().getResource("/login_scene.fxml"));
+            Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
